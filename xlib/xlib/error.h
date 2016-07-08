19fb669bfd576628bf24bac2fa4470933ed63458
kmichalk <kmichalkiewicz.95@gmail.com>
2016-07-08 17:53:41 +0200

    improved template hierarchy, added std::ostream<< operator

---
+++ xlib/xlib/error.h
@@ -0,0 +1,34 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <iostream>
+
+namespace x {
+
+template<typename = void>
+class error: public error<void>
+{
+public:
+	using error<void>::error;
+};
+
+
+template<>
+class error<void>
+{
+public:
+	const int num;
+	const char* message;
+
+	error(int num = 0, const char* message = ""):
+		num{num}, message{message}
+	{}
+
+	friend std::ostream& operator<<(std::ostream& os, error const& err)
+	{
+		return os<<"E"<<err.num<<": "<<err.message<<std::endl;
+	}
+};
+
+}
+#endif
