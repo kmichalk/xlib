e10028ff78211563675820ac54e68de2bf61ab85
kmichalk <kmichalkiewicz.95@gmail.com>
2016-07-08 18:18:31 +0200

    added until loop

--- xlib/xlib/loops.h
+++ xlib/xlib/loops.h
@@ -6,5 +6,6 @@
 #define repeat(n) for (int __i__=0;__i__<n;++__i__)
 #define foreach(i, cnt) for (size_t i = 0, __size=x::size(cnt); i<__size; ++i)
 #define loop while(true)
+#define until(test) while(! (test))
 
 #endif
