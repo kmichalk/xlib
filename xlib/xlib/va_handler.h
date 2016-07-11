2e20933030d8c68479360eb79c8fdf16906ec0d4
kmichalk <kmichalkiewicz.95@gmail.com>
2016-07-12 01:18:16 +0200

    variadic expression expander helper function expand moved here

--- xlib/xlib/va_handler.h
+++ xlib/xlib/va_handler.h
@@ -1,39 +1,104 @@
 #pragma once
 
 #define enable_if std::enable_if_t
+#include "more_type_traits.h"
 
-template<typename... T>
-class va_handler
+//template<typename... T>
+//class va_handler
+//{
+//	static_assert(all_same<T...>::value,
+//		"Arguments have to be of the same type.");
+//private:
+//	using type_ = x::select_t<1, T...>;
+//	type_* args_;
+//	size_t size_;
+//public:
+//	__forceinline size_t size() const
+//	{
+//		return size_;
+//	}
+//	va_handler(T... args) :
+//		size_{sizeof...(args)}
+//	{
+//		args_ = new type_[size_]{args...};
+//	}
+//	__forceinline type_& operator[](size_t i)
+//	{
+//		return args_[i];
+//	}
+//	type_& get(size_t i)
+//	{
+//		if (i<size_) {
+//			return args_[i];
+//		}
+//		throw "out of size";
+//	}
+//	~va_handler()
+//	{
+//		delete[] args_;
+//	}
+//};
+
+namespace x
 {
-	static_assert(all_same<T...>::value,
-		"Arguments have to be of the same type.");
-private:
-	using type_ = typename select<1, T...>::type;
-	type_* args_;
-	size_t size_;
-public:
-	__forceinline size_t size() const
+
+	template<typename... A>
+	class va_handler
 	{
-		return size_;
-	}
-	va_handler(T... args) :
-		size_{sizeof...(args)}
-	{
-		args_ = new type_[size_]{args...};
-	}
-	__forceinline type_& operator[](size_t i)
-	{
-		return args_[i];
-	}
-	type_& get(size_t i)
-	{
-		if (i<size_) {
-			return args_[i];
+	public:
+		static constexpr unsigned size = sizeof...(A);
+
+	private:
+		template<int i>
+		using ArgT_ = x::select_t<i, A...>;
+
+		class Eraser_ {};
+
+		template<typename T>
+		class Concrete_: public Eraser_
+		{
+		public:
+			T value;
+			Concrete_(T const& value):
+				value{value}
+			{
+			}
+		};
+
+		Eraser_* args_[size];
+
+		/*template<int... i>
+		void assign_(seq<i...>&, A const&... args)
+		{
+			args_ = new Eraser_*[size] { new Concrete_<A>{args}... };
+		}*/
+
+	public:
+		va_handler(A const&... args):
+			args_{(new Concrete_<A>{args}) ...}
+		{
 		}
-		throw "out of size";
-	}
-	~va_handler()
+
+		template<unsigned i>
+		__forceinline ArgT_<i>& get()
+		{
+			static_assert(i <= size,
+				"Index exceeds parameter pack size.");
+			return static_cast<Concrete_<ArgT_<i>>*>(args_[i-1])->value;
+		}
+
+		~va_handler()
+		{
+			for (int i = 0; i<size; ++i) delete args_[i];
+		}
+	};
+
+	template<typename... A>
+	inline va_handler<A...> unpack(A const&... args)
 	{
-		delete[] args_;
+		return va_handler<A...>{args...};
 	}
-};
+
+	template<typename... T>
+	__forceinline void expand(T...) {}
+}
