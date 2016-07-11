6cc6d679905fce75ed37c844609e862541f3394b
kmichalk <kmichalkiewicz.95@gmail.com>
2016-07-12 01:17:34 +0200

    added 2-side cutting function cutr. removed useless functions

--- xlib/xlib/simple.h
+++ xlib/xlib/simple.h
@@ -1,46 +1,49 @@
 #ifndef SIMPLE_H
 #define SIMPLE_H
 
-#include <cmath>
-
 #undef min
 #undef max
 
 namespace x
 {
 
-template<typename T1, typename T2>
-inline auto min(T1&& n1, T2&& n2)
-{
-	return n1 > n2 ? n1 : n2;
-}
+	template<typename T1, typename T2>
+	inline auto min(T1&& n1, T2&& n2)
+	{
+		return n1 > n2 ? n1 : n2;
+	}
 
-template<typename T1, typename T2>
-inline auto max(T1&& n1, T2&& n2)
-{
-	return n1 < n2 ? n1 : n2;
-}
+	template<typename T1, typename T2>
+	inline auto max(T1&& n1, T2&& n2)
+	{
+		return n1 < n2 ? n1 : n2;
+	}
 
-template<typename T1, typename T2 = int>
-inline auto cutl(T1&& n, T2&& limit = 0)
-{
-	return n < limit ? limit : n;
-}
-template<typename T1, typename T2 = int>
-inline auto cutle(T1&& n, T2&& limit = 0)
-{
-	return n <= limit ? limit : n;
-}
-template<typename T1, typename T2 = int>
-inline auto cutg(T1&& n, T2&& limit = 0)
-{
-	return n > limit ? limit : n;
-}
-template<typename T1, typename T2 = int>
-inline auto cutge(T1&& n, T2&& limit = 0)
-{
-	return n >= limit ? limit : n;
-}
+	template<typename T1, typename T2 = int>
+	inline auto cutl(T1&& n, T2&& limit = 0)
+	{
+		return n < limit ? limit : n;
+	}
+	//template<typename T1, typename T2 = int>
+	//inline auto cutle(T1&& n, T2&& limit = 0)
+	//{
+	//	return n <= limit ? limit : n;
+	//}
+	template<typename T1, typename T2 = int>
+	inline auto cutg(T1&& n, T2&& limit = 0)
+	{
+		return n > limit ? limit : n;
+	}
+	//template<typename T1, typename T2 = int>
+	//inline auto cutge(T1&& n, T2&& limit = 0)
+	//{
+	//	return n >= limit ? limit : n;
+	//}
+	template<typename T1, typename T2, typename T3>
+	inline auto cutr(T1&& n, T2&& a, T3&& b)
+	{
+		return n < a ? a : n > b ? b : n;
+	}
 
 }
 #endif //SIMPLE_H
