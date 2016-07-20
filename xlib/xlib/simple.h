3987ec117dede1ad758a35c9a80f05876a4a56c7
kmichalk <kmichalkiewicz.95@gmail.com>
2016-07-20 20:34:39 +0200

    mantissa and signum functions

--- xlib/xlib/simple.h
+++ xlib/xlib/simple.h
@@ -1,12 +1,15 @@
 #ifndef SIMPLE_H
 #define SIMPLE_H
 
+#include "more_type_traits.h"
+
 #undef min
 #undef max
 
+#define enable_if std::enable_if_t
+
 namespace x
 {
-
 	template<typename T1, typename T2>
 	inline auto min(T1&& n1, T2&& n2)
 	{
@@ -20,30 +23,51 @@ namespace x
 	}
 
 	template<typename T1, typename T2 = int>
-	inline auto cutl(T1&& n, T2&& limit = 0)
+	inline auto cutl(T1&& val, T2&& limit = 0)
 	{
-		return n < limit ? limit : n;
-	}
-	//template<typename T1, typename T2 = int>
-	//inline auto cutle(T1&& n, T2&& limit = 0)
-	//{
-	//	return n <= limit ? limit : n;
-	//}
-	template<typename T1, typename T2 = int>
-	inline auto cutg(T1&& n, T2&& limit = 0)
-	{
-		return n > limit ? limit : n;
-	}
-	//template<typename T1, typename T2 = int>
-	//inline auto cutge(T1&& n, T2&& limit = 0)
-	//{
-	//	return n >= limit ? limit : n;
-	//}
-	template<typename T1, typename T2, typename T3>
-	inline auto cutr(T1&& n, T2&& a, T3&& b)
-	{
-		return n < a ? a : n > b ? b : n;
+		return val < limit ? limit : val;
 	}
 
+	template<typename T1, typename T2 = int>
+	inline auto cutg(T1&& val, T2&& limit = 0)
+	{
+		return val > limit ? limit : val;
+	}
+
+	template<typename T1, typename T2, typename T3>
+	inline auto cutr(T1&& val, T2&& a, T3&& b)
+	{
+		return val < a ? a : val > b ? b : val;
+	}
+
+	template <typename T>
+	inline enable_if<std::is_signed<T>::value,
+		int> sgn(T val)
+	{
+		return (T(0) < val) - (val < T(0));
+	}
+
+	template <typename T>
+	inline enable_if<std::is_unsigned<T>::value,
+		int> sgn(T val)
+	{
+		return bool(val);
+	}
+
+	template<typename T>
+	inline enable_if<std::is_floating_point<T>::value, 
+		T> mant(T val)
+	{
+		return val-int(val);
+	}
+
+	template<typename T, typename = enable_if<true>>
+	inline enable_if<std::is_integral<T>::value, 
+		double> mant(T val)
+	{
+		return 0;
+	}
 }
+
+#undef enable_if
 #endif //SIMPLE_H
